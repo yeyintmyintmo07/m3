da219ccd81f1825ca64ac491cb2837b8ba2564f8
Wai Yan Myint Mo <dragonkey100@gmail.com>
2019-12-16 01:43:58 +0630

    [CONSTRING] Connection string added for 000webhost

--- connection.php
+++ connection.php
@@ -1,9 +1,17 @@
-<?php 
+<?php
 
+    //Local Dev
     $host_name = "localhost";
     $mysql_username = "root";
     $mysql_password = "";
     $mysql_dbname = "m3";
 
+    //000WebHost SQL Account Login Details
+    /*
+    $host_name = "localhost";
+    $mysql_username = "id11951589_root";
+    $mysql_password = "password";
+    $mysql_dbname = "id11951589_m3";
+    */
+
     $con = mysqli_connect($host_name, $mysql_username, $mysql_password, $mysql_dbname);
-?>
